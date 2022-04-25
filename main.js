01d342f5b752afaa2f67eb916fe697632c7a8cfa
TT12345OPPOGO <96768087+TT12345OPPOGO@users.noreply.github.com>
2022-04-25 17:40:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+video = "";
+Status = "";
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 380, 380)
+    if(Status !="")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill("#40E0D0")
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("40E0D0")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+
+            if(objects[i].label == object_name)
+            {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("object_status").innerHTML = object_name + "found";
+                synth = window.speechSynthesis;
+                utterThis = new SpeechSynthesisUtterance(object_name + "found");
+                synth.speak(utterThis);
+            }
+            else
+            {
+                document.getElementById("object_status").innerHTML = object_name + " Not Found";
+            }
+        }
+    }
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+    Status = true;
+}
+
+function gotResult(error, results)
+{
+    if  (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
